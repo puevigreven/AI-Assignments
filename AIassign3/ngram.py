89f9cc5ecd548015045c39a489bc70c76b2c1607
puevigreven <pralay22@gmail.com>
2017-10-08 23:22:03 +0530

    day2 work

--- AIassign3/ngram.py
+++ AIassign3/ngram.py
@@ -2,52 +2,58 @@ import nltk
 from nltk import bigrams
 from nltk import trigrams
 
-# text="""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare
-# tempor lacus, quis pellentesque diam tempus vitae. Morbi justo mauris,
-# congue sit amet imperdiet ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare
-# tempor lacus, quis pellentesque diam"""
-# split the texts into tokens
-with open('test_final.txt', 'r') as myfile:
-    text=myfile.read()
-# type(data)
+def find_n_grams(source, des) :
+    """
+    Change in .write function while dealing with Train data.
+    """
+    with open(source, 'r') as myfile:
+        text=myfile.read()
+    # type(data)
 
-unigram_file = open("test_unigram.txt", "w")
-bigram_file = open("test_bigram.txt", "w")
-trigram_file = open("test_trigram.txt", "w")
+    unigram_file = open(des, "w")
+    # bigram_file = open("ABBR_bigram.txt", "w")
+    # trigram_file = open("ABBR_trigram.txt", "w")
 
-tokens = nltk.word_tokenize(text)
-tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
-bi_tokens = bigrams(tokens)
-tri_tokens = trigrams(tokens)
-trigramToken = list(tri_tokens)
-bigramToken = list(bi_tokens)
-
-
-unigrmdic = {}
-for item in sorted(set(tokens)):
-	unigrmdic[item] = tokens.count(item)
-for key, value in sorted(unigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
-    print "%s: %s" % (key, value)
-    unigram_file.write("%s: %s \n" % (key, value))
-unigram_file.close()
-
-bigrmdic = {}
-for item in sorted(set(bigramToken)):
-	bigrmdic[item] = bigramToken.count(item)
-for key, value in sorted(bigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
-    print "%s: %s" % (key, value)
-    bigram_file.write("%s: %s \n" % (key, value))
-bigram_file.close()
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
+    bi_tokens = bigrams(tokens)
+    tri_tokens = trigrams(tokens)
+    trigramToken = list(tri_tokens)
+    bigramToken = list(bi_tokens)
 
 
-    
+    unigrmdic = {}
+    for item in sorted(set(tokens)):
+        unigrmdic[item] = tokens.count(item)
+    for key, value in sorted(unigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
+        print "%s: %s" % (key, value)
+        unigram_file.write("%s \n" % (key))
+    # unigram_file.close()
 
-trigrmdic = {}
-for item in sorted(set(trigramToken)):
-	trigrmdic[item] = trigramToken.count(item)
-for key, value in sorted(trigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
-    print "%s: %s" % (key, value)
-    trigram_file.write("%s: %s \n" % (key, value))
-trigram_file.close()
+    bigrmdic = {}
+    for item in sorted(set(bigramToken)):
+        bigrmdic[item] = bigramToken.count(item)
+    for key, value in sorted(bigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
+        print "%s: %s" % (key, value)
+        unigram_file.write("%s \n" % str(key))
+    # bigram_file.close()
+
+    trigrmdic = {}
+    for item in sorted(set(trigramToken)):
+        trigrmdic[item] = trigramToken.count(item)
+    for key, value in sorted(trigrmdic.iteritems(), key=lambda (k,v): (v,k) ,reverse=True):
+        print "%s: %s" % (key, value)
+        unigram_file.write("%s \n" % str(key))
+    unigram_file.close()
+
+
+find_n_grams('NUM-Group.txt' , 'NUM_ngram.txt')
+find_n_grams('LOC-Group.txt' , "LOC_ngram.txt")
+find_n_grams('HUM-Group.txt' , "HUM_ngram.txt")
+find_n_grams('ABBR-Group.txt' , "ABBR_ngram.txt")
+find_n_grams('DESC-Group.txt' , "DESC_ngram.txt")
+find_n_grams('ENTY-Group.txt' , "ENTY_ngram.txt")
+
+
 
 
--- AIassign3/removepuntuation.py
+++ AIassign3/removepuntuation.py
@@ -1,17 +1,19 @@
 import string 
 
-# s = "hello, ? ; "
+def removePuntuation(source , destination):
+	with open(source, 'r') as myfile:
+		data=myfile.read()
 
+	s = data.translate(None, string.punctuation)
+	# print s
 
+	text_file = open(destination, "w")
+	text_file.write(s)
+	text_file.close()
 
-
-with open('trainC.txt', 'r') as myfile:
-    data=myfile.read()
-
-s = data.translate(None, string.punctuation)
-
-# print s
-
-text_file = open("final_train.txt", "w")
-text_file.write(s)
-text_file.close()
+removePuntuation('ABBR-Group.txt' , 'ABBR-Group.txt')
+removePuntuation('HUM-Group.txt' , 'HUM-Group.txt')
+removePuntuation('ENTY-Group.txt' , 'ENTY-Group.txt')
+removePuntuation('LOC-Group.txt' , 'LOC-Group.txt')
+removePuntuation('NUM-Group.txt' , 'NUM-Group.txt')
+removePuntuation('DESC-Group.txt' , 'DESC-Group.txt')
